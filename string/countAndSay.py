b3d5b3023326005ccb3a3e171a5ca738fbe753ee
Xu <xuhaixiang103@163.com>
2018-07-17 22:13:37 +0800

    编写一个函数来查找字符串数组中的最长公共前缀。

--- string/countAndSay.py
+++ string/countAndSay.py
@@ -15,9 +15,31 @@ def countAndSay(n):
             output1 = output1 + str(count+1) + str(output[j])
             count = 0
             j = j+1
-
         output = output1
     return output
+
+def countAndSayBestSummit(n):
+    """
+    :type n: int
+    :rtype: str
+    """
+    index = 1
+    string = '1'
+    while index < n:
+        num = 0
+        nextStr = ''
+        prech = string[0]
+        for ch in string:
+            if ch == prech:
+                num += 1
+            else:
+                nextStr += (str(num)+prech)
+                num = 1
+                prech = ch
+        nextStr += (str(num)+prech)
+        string = nextStr
+        index += 1
+    return string
 
 if __name__ == "__main__":
     input = 6
